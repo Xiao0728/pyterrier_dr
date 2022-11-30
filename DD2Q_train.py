56fe9ad694b8f05b309ea4f18ebfaec512e986d4
Xiao0728 <x.wang.8@research.gla.ac.uk>
2022-11-30 19:27:54 +0000

    dd2q training

--- DD2Q_train.py
+++ DD2Q_train.py
@@ -21,19 +21,6 @@ parser.add_argument('--evalRes-path', default = './res-tct', type=str, help = 's
 parser.add_argument('--in_batch_negs', default = False, action='store_true')
 parser.add_argument('--bsize', default=8, type=int, help='train batch size')
 
-temperature = 0.25
-# in_batch_negs = True
-# batch_size =8
-def processQuery(query):
-    query = re.sub(r"[^a-zA-Z0-9¿]+", " ", query)
-    return query
-
-def clean(inputDF):
-    inputDF['query']=inputDF['query'].apply(lambda query: processQuery(query))
-    return inputDF
-
-
-
 
 def main():
     args = parser.parse_args()
@@ -53,123 +40,80 @@ def main():
     optimizer = AdamW([p for p in dd2q_model.model.parameters() if p.requires_grad], lr=1e-5, eps=1e-8)
     optimizer.zero_grad()
     # loss_fn = nn.KLDivLoss(reduction='batchmean')
-    loss_fn_ce = nn.CrossEntropyLoss()
-    loss_fn_mse = nn.MSELoss()
+    # loss_fn_ce = nn.CrossEntropyLoss()
+    # loss_fn_mse = nn.MSELoss()
 
-
+    loss_fn = nn.CrossEntropyLoss()
     # dataset = pt.get_dataset('irds:msmarco-passage/train/triples-small')
     # 
-    dataset = pt.get_dataset('irds:msmarco-passage/train/split200-train')
+    dataset = pt.get_dataset('irds:msmarco-passage')
     irds = dataset.irds_ref()
-    # valid_dataset = pt.get_dataset('irds:msmarco-passage/train/split200-valid')
-    # For trec 2019 to evaluate
-    # valid_dataset = pt.get_dataset('irds:msmarco-passage/trec-dl-2019/judged')
-    # valid_irds = valid_dataset.irds_ref()
-    # # valid_data = pd.DataFrame(valid_irds.scoreddocs).rename(columns={'query_id': 'qid', 'doc_id': 'docno'})
-    # # valid_data = (pt.apply.query_text(lambda x: valid_irds.queries.lookup(x['qid']).text) >> pt.text.get_text(valid_dataset, 'text'))(valid_data)
-    # # valid_data = valid_data.rename(columns={'query_text': 'query'})
-
-
-    # For DEV evaluation
-    
-    # dev_dataset = pt.get_dataset('irds:msmarco-passage/dev/small')
-    # irds = dataset.irds_ref()
-    # dev_irds = dataset.irds_ref()
-    # dev_data = pd.DataFrame(dev_irds.scoreddocs).rename(columns={'query_id': 'qid', 'doc_id': 'docno'})
-    # dev_qrels = dev_dataset.get_qrels()
-    # dev_data_t100 = (dev_data.head(200)).merge(dev_qrels[dev_qrels["label"]>0][["qid"]].drop_duplicates()).head(100)
-    # dev_data_t100 = (pt.apply.query_text(lambda x: dev_irds.queries.lookup(x['qid']).text) >> pt.text.get_text(dev_dataset, 'text'))(dev_data_t100)
-    # dev_data_t100 = dev_data_t100.rename(columns={'query_text': 'query'})
+   
 
     ref_model.model.eval()
-    pair_it = iter(irds.docpairs)
-    tct_ndcg_max =0.0
-    for outer_i in logger.pbar(range(500), desc='validation loop'):
-        running_losses, running_accs = [], []
-        dd2q_model.model.train()
-        with logger.pbar_raw(desc='train loop', total=1000) as pbar:
-            for i, chunk in enumerate(chunked(pair_it, args.bsize//2)):
+    pair_it = iter(irds.docs) # contain: d
+    num_ref = 80
+    running_losses, running_accs= [],[]
+    dd2q_model.model.train()
+    with logger.pbar_raw(desc='train loop', total=1000) as pbar:
+            for i, chunk in enumerate(chunked(pair_it, args.bsize)):
                 if i == 1000:
                     break
-                batch_q, batch_d, batch_ref = [], [], [] 
-                for qid, pos_did, neg_did in chunk:
-                    q = irds.queries.lookup(qid).text
-                    batch_q.append(q)
-                    if not args.in_batch_negs:
-                        batch_q.append(q)
-                    batch_d.append(irds.docs.lookup(pos_did).text)
-                    batch_d.append(irds.docs.lookup(neg_did).text)
-                    # for each training query, we retrieve from the d2q index, and obtain the text from dataset; function to merge them 
-                    # obtain the full_text: ref_query + PRF texts:
-                    # encode by the PRF encoder;
-                    # 
+                batch_ref, batch_d = [], [] 
+                for doc in chunk:
+                    doc_id = doc.doc_id
+                    batch_d.append(doc.text)
+                    
+                    ref = references_dictionary(doc_id)# TODO: obtain all the references according to the current doc_id also control the number of reference for each docid.
+                    batch_ref.append(ref)
+                with torch.no_grad():
+                    batch_ref = ref_model.tokenizer([f'{q}'+ ' '.join(['[MASK]'] * 32) for q in batch_ref],add_special_tokens=False, return_tensors='pt', padding=True, truncation=True, max_length=512) 
+                    batch_ref = {k: v.cuda() for k,v in batch_ref.items()}
+                    R = ref_model.model(**batch_ref).last_hidden_state
 
-                    ref_qs = d2q_dict['pos_did'].split(".")
-                    for q in ref_qs:
-                        batch_ref.append(q)
-                    batch_ref.append(irds.docs.lookup(pos_did).text) # batch_r #TODO-1: how to obtain d2q generated queries, by given the pos/neg document ids?
-
-                # with torch.no_grad():
-
-                #     colbert_scores = colbert_model.model.forward_score(batch_q, batch_d, all_pairs=args.in_batch_negs)
-                # if args.in_batch_negs:
-                #     colbert_scores = colbert_scores
-                # else:
-                #     colbert_scores = colbert_scores.reshape(-1, 2)
-                
-                # colbert_probs = (colbert_scores/temperature).softmax(dim=1)
-            
-                # this line of code is wrong
-                batch_ref = ref_model.tokenizer([f'[CLS] [Q] {q} ' + ' '.join(['[MASK]'] * 32) for q in batch_ref], add_special_tokens=False, return_tensors='pt', padding=True, truncation=True, max_length=512)
-                batch_q = dd2q_model.tokenizer([f'[CLS] [Q] {q} ' + ' '.join(['[MASK]'] * 32) for q in batch_q], add_special_tokens=False, return_tensors='pt', padding=True, truncation=True, max_length=36)
-                batch_d = dd2q_model.tokenizer([f'[CLS] [D] {d}' for d in batch_d], add_special_tokens=False, return_tensors='pt', padding=True, truncation=True, max_length=256)
-                batch_q = {k: v.cuda() for k, v in batch_q.items()}
+                # the numeber of [MASK] tokens = the number of references for each docid
+                batch_d = dd2q_model.tokenizer(['[CLS] '* num_ref + f'{d}' for d in batch_d], add_special_tokens=False, return_tensors='pt', padding=True, truncation=True, max_length=512)
                 batch_d = {k: v.cuda() for k, v in batch_d.items()}
-                batch_ref = {k: v.cuda() for k,v in batch_ref.items()}
-
-
-                R = ref_model.model(**batch_ref).last_hiddent_state
-                R = R[:, 4:, :].mean(dim=1) # same with the TCT-PRF/or ANCE-PRF 
-
-
-
-                Q = dd2q_model.model(**batch_q).last_hidden_state
-                Q = Q[:, 4:, :].mean(dim=1) # remove the first 4 tokens (representing [CLS] [ Q ]), and average
 
                 D = dd2q_model.model(**batch_d).last_hidden_state
+                D = D[:, :num_ref, :] # take the first num_ref [CLS] embeddings as the view embeddings
+
+
+                matrix = torch.einsum('rie, die', R, D)
+                scores_row_wise= matrix.max(dim=2).values.sum(dim=1) # for a given column,i.e. the given reference, obtain the maximum scored documents
+                scores_col_wise = matrix.max(dim=2).values.sum(dim=0) 
+
+
+                # TODO: then we need targets for socres_row_wise, and scores_col_wise
+                # TODO: then, we have two types of loss. the recall loss and the prevision loss, calculated using the 
+                #  row_wise and col_wise, be careful here
+                # TODO: the losses below are wrong, haven't thought them carefully
+
+                targets_row_wise = torch.zeros_like(scores_row_wise[:, 0]).long() 
+                loss_row_wise = loss_fn(scores_row_wise, targets_row_wise)
+                # acc_row_wise = ((scores_row_wise.max(dim=1).indices == targets_row_wise).sum) / scores_row_wise.shape[0].cpu().detach().item()
                 
-                loss_minsim = loss_fn_mse(R,D)
+                targets_col_wise = torch.zeros_like(scores_col_wise[:, 0]).long() 
+                loss_col_wise = loss_fn(scores_col_wise, targets_col_wise)
+                # acc_col_wise = ((scores_row_wise.max(dim=1).indices == targets_row_wise).sum) / scores_row_wise.shape[0].cpu().detach().item()
                 
+                loss = loss_row_wise + loss_col_wise
 
 
 
-
-                D_ce = D[:, 4:, :] # remove the first 4 tokens (representing [CLS] [ D ])
-                D_ce = D_ce * batch_d['attention_mask'][:, 4:].unsqueeze(2) # apply attention mask
-                D_ce = D_ce.sum(dim=1) / batch_d['attention_mask'][:, 4:].sum(dim=1).unsqueeze(1) # average based on dim
-
-
-                if args.in_batch_negs:
-                    scores = torch.einsum('qe,de->qd', Q, D_ce)
-                    # targets = (torch.arange(batch_q['input_ids'].shape[0]) * 2).to(tct_scores.device)
-                else:
-                    scores = torch.einsum('be,be->b', Q, D_ce).reshape(-1, 2)
-                
-
-                logprobs = nn.functional.log_softmax(scores, dim=1)
-                loss_ce = loss_fn_ce(logprobs)
-                loss = loss_minsim + loss_ce
-
-
+                loss.backward()
                 loss = loss.cpu().detach().item()
                 optimizer.step()
                 optimizer.zero_grad()
                 pbar.update()
                 running_losses.append(loss)
-                
-                pbar.set_postfix({'loss': loss})
+                running_accs.append(acc)
+                pbar.set_postfix({'loss': loss, 'acc': acc})
+                if len(running_losses) == 100:
+                    logger.info(f'it={i+1} loss={sum(running_losses)/len(running_losses)} acc={sum(running_accs)/len(running_accs)}')
+                    running_losses, running_accs = [], []
 
-                writer.add_scalar("train/1.loss", loss, outer_i)
+                # writer.add_scalar("train/1.loss", loss, outer_i)
                 
                
 
@@ -178,41 +122,6 @@ def main():
                     # running_losses, running_accs = [], []
             writer.add_scalar("train/3.runningloss", sum(running_losses[-1000:])/len(running_losses[-1000:]), outer_i)   
             
-
-
-        dd2q_model.model.eval()
-        # results = pt.Experiment([tct_model], valid_data, valid_dataset.get_qrels(), [nDCG@10, RR(rel=2), AP(rel=2), Judged@10])
-        # topicsDev100 = pd.read_csv("./topicsDev_t100.txt")
-        topicsDev100 = pd.read_csv("./split200_topics.txt")
-        # topicsDev100.qid = topicsDev100.qid.astype(str)
-        # topicsDev100.docno = topicsDev100.docno.astype(str)
-        qrels = pd.read_csv("./qrels_split_200.txt")
-        results = pt.Experiment([tct_model], topicsDev100, qrels, [nDCG@10, RR(rel=2), AP(rel=2), Judged@10]) 
-        # results = pt.Experiment([tct_model], topicsDev100, pt.get_dataset("trec-deep-learning-passages").get_qrels('dev.small'),[nDCG@10, RR(rel=2), AP(rel=2), Judged@10])
-        print(outer_i, results)
-
-        tct_ndcg = results.iloc[0]["nDCG@10"]
-        writer.add_scalar("eval/1.tct.ndcg10", tct_ndcg, outer_i)
-        writer.flush()
-        if results.iloc[0]["nDCG@10"] > tct_ndcg_max: 
-            tct_ndcg_max = results.iloc[0]["nDCG@10"]
-            print(f" New Max NDCG@10 score reached at epoch{outer_i} with NDCG@10 score = {tct_ndcg_max}") 
-            if args.in_batch_negs:
-                dd2q_model.model.save_pretrained(f'/nfs/sean/workspace_xiao/pyterrier_sbert/checkpoint/tct-ibn-b{args.bsize}_dev/tct-ibn-{outer_i}-{tct_ndcg_max}')
-                dd2q_model.tokenizer.save_pretrained(f'/nfs/sean/workspace_xiao/pyterrier_sbert//checkpoint/tct-ibn-b{args.bsize}_dev/tct-ibn-{outer_i}-{tct_ndcg_max}') 
-            else:
-                dd2q_model.model.save_pretrained(f'/nfs/sean/workspace_xiao/pyterrier_sbert/checkpoint/tct-pair-b{args.bsize}_dev/tct-pair-{outer_i}-{tct_ndcg_max}')
-                dd2q_model.tokenizer.save_pretrained(f'/nfs/sean/workspace_xiao/pyterrier_sbert/checkpoint/tct-pair-b{args.bsize}_dev/tct-pair-{outer_i}-{tct_ndcg_max}')
-        if args.in_batch_negs:
-            with open(f'./eval_res_26Jan/tct-ibn_bsize{args.bsize}_dev100.txt', 'at') as fout:
-                fout.write(f'{outer_i} {args.bsize} {results}\n')
-            # tct_model.model.save_pretrained(f'./checkpoint/tct-ibn/tct-ibn-{outer_i}')
-            # tct_model.tokenizer.save_pretrained(f'./checkpoint/tct-ibn/tct-ibn-{outer_i}')
-        else:
-            with open(f'./eval_res_26Jan/tct-pair_bsize{args.bsize}_dev100.txt', 'at') as fout:
-                fout.write(f'{outer_i} {args.bsize} {results}\n')
-            # tct_model.model.save_pretrained(f'./checkpoint/tct-pair/tct-pair-{outer_i}')
-            # tct_model.tokenizer.save_pretrained(f'./checkpoint/tct-pair/tct-pair-{outer_i}')
 
 
 if __name__ == '__main__':
--- pyterrier_dr/dd2q_model.py
+++ pyterrier_dr/dd2q_model.py
@@ -18,7 +18,7 @@ parser.add_argument('--in_batch_negs', default = False, action='store_true')
 parser.add_argument('--bsize', default=8, type=int, help='train batch size')
 
 class DD2Q(pt.Transformer):
-    def __init__(self, model_name='castorini/tct_colbert-msmarco', batch_size=32, text_field='text', verbose=False, cuda=None):
+    def __init__(self, model_name='castorini/tct_colbert-msmarco', num_ref=80, batch_size=32, text_field='text', verbose=False, cuda=None):
         self.model_name = model_name
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.cuda = torch.cuda.is_available() if cuda is None else cuda
@@ -34,7 +34,7 @@ class DD2Q(pt.Transformer):
             (['qid', 'query'], self._transform_Q),
             (['docno', self.text_field], self._transform_D),
         ]
-        
+        self.num_ref = num_ref
 
     def transform(self, inp):
         columns = set(inp.columns)
@@ -46,22 +46,12 @@ class DD2Q(pt.Transformer):
         for fields, fn in self.modes:
             f += f'\n - {fn.__doc__.strip()}: {columns}\n'
         raise RuntimeError(message)
-    # TODO: change the training code fit function:
-    """
-    1. there are there encoders, (a) query encoder, (b). doc-encoder, (c) reference d2qquery encoder
-    2. structure: encoder (c) produce the d2q embeddings: in some way as what our baseline did: e.g. anceprf encoder?
-    3. then, (a) query encoder encode the queries, (b) doc encoder encode the documents; 
-    4. design the function using: MinSim (embeddings from (c), embeddings from (b));
-    5. train the model, report the validation performance on dl2019.
-    question: 
-    i: which query encoder are we going to use as (a) and (b)? ANCE or TCT-ColBERT?
-    ii: do we need hard-queries for training?
-
-    """
+   
+  
     
     def _encode_queries(self, texts):
         """
-        Query vectorisation: use TCT query encoder?
+        Query vectorisation: use ANCE query encoder: input: reference shape:[batch_size x num_ref] output: ref_embs, shape: [batch_size x num_ref * dim (768)]  
         """
         results = []
         with torch.no_grad():
@@ -69,10 +59,10 @@ class DD2Q(pt.Transformer):
                 inps = self.tokenizer([f'[CLS] [Q] {q} ' + ' '.join(['[MASK]'] * 32) for q in chunk], add_special_tokens=False, return_tensors='pt', padding=True, truncation=True, max_length=36)
                 if self.cuda:
                     inps = {k: v.cuda() for k, v in inps.items()}
-                res = self.model(**inps).last_hidden_state
-                res = res[:, 4:, :].mean(dim=1) # remove the first 4 tokens (representing [CLS] [ Q ]), and average
-                results.append(res.cpu().numpy())
+                results = self.model(**inps).cpu().numpy()
+
         return np.concatenate(results, axis=0)
+
 
     def _encode_docs(self, texts):
         """
@@ -81,17 +71,37 @@ class DD2Q(pt.Transformer):
         results = []
         with torch.no_grad():
             for chunk in chunked(texts, self.batch_size):
-                inps = self.tokenizer([f'[CLS] [D] {d}' for d in chunk], add_special_tokens=False, return_tensors='pt', padding=True, truncation=True, max_length=512)
+                doc_text =['[CLS] '* self.num_ref +' [SEP] '  + f' {d}' for d in chunk]
+                print(">>>>> doc_text",doc_text)
+                inps = self.tokenizer(doc_text, add_special_tokens=False, return_tensors='pt', padding=True, truncation=True, max_length=512)
+                
                 if self.cuda:
                     inps = {k: v.cuda() for k, v in inps.items()}
+
                 res = self.model(**inps).last_hidden_state
-                res = res[:, 4:, :] # remove the first 4 tokens (representing [CLS] [ D ])
-                res = res * inps['attention_mask'][:, 4:].unsqueeze(2) # apply attention mask
-                lens = inps['attention_mask'][:, 4:].sum(dim=1).unsqueeze(1)
-                lens[lens == 0] = 1 # avoid edge case of div0 errors
+                res = res[:, :self.num_ref,:] # only takes the first num_prf cls embeddings as the doc_view embeddings to learn
+                
+                
+                # TODO: do we need the below codes?
+                res = res * inps['attention_mask'][:, :self.num_ref].unsqueeze(2) # apply attention mask
+                lens = inps['attention_mask'][:, :self.num_ref].sum(dim=1).unsqueeze(1) 
+                lens[lens==0]=1 
                 res = res.sum(dim=1) / lens # average based on dim
                 results.append(res.cpu().numpy())
+
         return np.concatenate(results, axis=0)
+    def forward_score(self, refs, docs, return_matrix=False):
+        r_embs = self._encode_queries(refs) #[BATCH_SIZE, NUM_REF, DIM]
+        d_embs = self._encode_docs(docs) #[BATCH_SIZE, NUM_REF [MASK]S, DIM]
+        matrix = torch.einsum('rie, die', r_embs, d_embs)
+        scores_row_wise= matrix.max(dim=2).values.sum(dim=1) # for a given column,i.e. the given reference, obtain the maximum scored documents
+        scores_col_wise = matrix.max(dim=2).values.sum(dim=0) # for a given row, i.e. the given document [MASK] view, obtain the maxium scored reference embedding;
+        if return_matrix:
+            return scores_row_wise, scores_col_wise, matrix
+        return scores_row_wise, scores_col_wise
+
+
+
 
     def _transform_D(self, inp):
         """
@@ -126,79 +136,3 @@ class DD2Q(pt.Transformer):
     
 
         
-        
-class TCTPRF(TctColBert):
-    """
-    in the following is the query encoder: TCTPRF
-    """
-    def __init__(self, prf_type = "anceprf",k=3, return_docs=False,model_name='/nfs/xiao/TCT_PRF/checkpoint',cuda=None):
-        super().__init__(model_name=model_name)
-        self.k = k
-        self.return_docs = return_docs
-
-        if prf_type == "anceprf":
-            """
-            anceprf: a new encoder: input = [query + 3PRF doc_texts], as new query
-            """
-            self.modes = [
-                (['qid', 'query', 'docno', self.text_field], self._transform_R_PRF)
-            ]
-        elif prf_type == "vectorprf":
-            """
-            vectorprf: take the average 
-            """
-            pass
-        elif prf_type == "rocchioprf":
-            """
-            take the self.alpha*query_vec + 
-            """
-            pass
-
-    def _transform_R_PRF(self, inp):
-        assert "docno" in inp.columns
-        assert "qid" in inp.columns
-        assert "query" in inp.columns 
-        assert "text" in inp.columns
-        assert self.text_field in inp.columns
-
-        new_qids = []
-        new_query_embs = []
-        iter = inp.groupby("qid")
-        iter = pt.tqdm(iter, desc='PRF', unit='q') if self.verbose else iter
-        for qid, group in iter:
-            k = min(self.k, len(group))
-            passage_texts = group.sort_values("rank").head(k)[self.text_field].values
-            passage_texts = [group.iloc[0].query] + passage_texts
-            #this line from APR: https://github.com/ielab/APR/blob/2b193113db7a50e666f31614b4ce1f1580f72212/pyserini/pyserini/dsearch/_prf.py#L310
-            full_text =  f'{self.tokenizer.sep_token.join(passage_texts)}'
-            #this line from pyserini
-#             full_text = f'{self.tokenizer.cls_token}{self.tokenizer.sep_token.join(passage_texts)}{self.tokenizer.sep_token}'
-            
-            new_qmeb = self._encode_queries(full_text)
-            new_qids.append(qid)
-            new_query_embs.append( np.squeeze(new_qmeb) )
-        qembs_df = pd.DataFrame(data={'qid' : new_qids, 'query_vec' : new_query_embs})
-
-        if self.return_docs:
-            rtr = inp[["qid","query","docno","text"]].merge(qembs_df,on='qid')
-        else:
-            rtr = inp[["qid", "query"]].drop_duplicates().merge(qembs_df, on='qid')        
-        return rtr    
-
-    
-
-    def encode(self, prf_query : str):    
-        import transformers
-        inputs = self.tokenizer(
-            [prf_query],
-            max_length=512,
-            padding='longest',
-            truncation=True,
-            add_special_tokens=False,
-            return_tensors='pt'
-        )  
-       #inputs = inputs.to(cuda)
-        inputs = inputs.to(self.model.device)
-        embeddings = self.model(inputs["input_ids"], inputs["attention_mask"]).detach().cpu().numpy()
-        return embeddings
-    
